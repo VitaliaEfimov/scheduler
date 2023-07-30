b63a479f3d678327566d793f6f3a3c1a475b8577
Vitaly Efimov <footballervit@mail.ru>
2023-07-30 12:21:52 +0700

    create update, add validation

--- src/main/java/ru/efimov/scheduler/controller/CategoryController.java
+++ src/main/java/ru/efimov/scheduler/controller/CategoryController.java
@@ -1,16 +1,21 @@
 package ru.efimov.scheduler.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.efimov.scheduler.entity.Category;
 import ru.efimov.scheduler.repository.CategoryRepository;
 
 import java.util.List;
 
+import static io.micrometer.common.util.StringUtils.isBlank;
+import static java.util.Objects.isNull;
+
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
 
-    private CategoryRepository categoryRepository;
+    private final CategoryRepository categoryRepository;
 
     public CategoryController(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
@@ -27,8 +32,32 @@ public class CategoryController {
     }
 
     @PostMapping("/add")
-    public Category add(@RequestBody Category category) {
-        return categoryRepository.save(category);
+    public ResponseEntity<?> add(@RequestBody Category category) {
+
+        if (!isNull(category.getId()) && category.getId() != 0) {
+            return new ResponseEntity<>("redundant param: id MUST be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (isBlank(category.getTitle())) {
+            return new ResponseEntity<>("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+
+        return ResponseEntity.ok(categoryRepository.save(category));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> update(@RequestBody Category category) {
+
+        if (isNull(category.getId()) || category.getId() == 0) {
+            return new ResponseEntity("missed param: id", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (isBlank(category.getTitle())) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(categoryRepository.save(category));
     }
 
 }
--- src/main/java/ru/efimov/scheduler/controller/PriorityController.java
+++ src/main/java/ru/efimov/scheduler/controller/PriorityController.java
@@ -1,16 +1,21 @@
 package ru.efimov.scheduler.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.efimov.scheduler.entity.Priority;
 import ru.efimov.scheduler.repository.PriorityRepository;
 
 import java.util.List;
 
+import static io.micrometer.common.util.StringUtils.isBlank;
+import static java.util.Objects.isNull;
+
 @RestController
 @RequestMapping("/priority")
 public class PriorityController {
 
-    private PriorityRepository priorityRepository;
+    private final PriorityRepository priorityRepository;
 
     public PriorityController(PriorityRepository priorityRepository) {
         this.priorityRepository = priorityRepository;
@@ -25,7 +30,35 @@ public class PriorityController {
     }
 
     @PostMapping("/add")
-    public Priority add(@RequestBody Priority priority) {
-        return priorityRepository.save(priority);
+    public ResponseEntity<?> add(@RequestBody Priority priority) {
+
+        if (!isNull(priority.getId()) && priority.getId() != 0) {
+            return new ResponseEntity<>("redundant param: id MUST be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (isBlank(priority.getTitle())) {
+            return new ResponseEntity<>("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+
+        return ResponseEntity.ok(priorityRepository.save(priority));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> update(@RequestBody Priority priority) {
+
+        if (isNull(priority.getId()) || priority.getId() == 0) {
+            return new ResponseEntity("missed param: id", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (isBlank(priority.getTitle())) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (isBlank(priority.getColor())) {
+            return new ResponseEntity("missed param: color", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(priorityRepository.save(priority));
     }
 }
